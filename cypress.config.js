c811f2caad7b1b29e6ffd7460058646796482882
askeledz <andrej.skeledzija@gmail.com>
2023-07-07 15:51:16 +0200

    Reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
