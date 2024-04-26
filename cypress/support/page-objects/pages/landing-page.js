320e27cd0d1e8985997102a0c12d59d3489d744c
Andrej Skeledzija <andrej.skeledzija@gmail.com>
2024-04-26 05:38:40 +0200

    Update landing-page.js

--- cypress/support/page-objects/pages/landing-page.js
+++ cypress/support/page-objects/pages/landing-page.js
@@ -2,7 +2,7 @@ export class landingPage {
 
   elements = {
     help: () => cy.get('.nav-button__help'),
-    signUp: () => cy.get('.nav-button__sign-up .button--underline'),
+    signUp: () => cy.get('.nav-button__sign-up .button--underline-x'),
     signIn: () => cy.get('.nav-button__sign-in .button--underline')
   }
 
